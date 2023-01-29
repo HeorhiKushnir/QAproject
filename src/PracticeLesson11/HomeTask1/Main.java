098ae29805236fe2a9324e66e4134516b13f9cef
Heorhi Kushnir <reiybhujif@mail.ru>
2023-01-29 23:02:05 +0300

    Lesson11: Homework task 1 - 3

---
+++ src/PracticeLesson11/HomeTask1/Main.java
@@ -0,0 +1,17 @@
+package PracticeLesson11.HomeTask1;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String str = "7382-ReS-9372-kHh-6L4d";
+
+        LineMethods.method1(str);
+        LineMethods.method2(str);
+        System.out.println(LineMethods.method3(str));;
+        LineMethods.method4(str);
+        LineMethods.method5(str);
+        LineMethods.method6(str);
+        LineMethods.method7(str);
+    }
+}
